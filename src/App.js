3a7a67564d63c2ae72b12a8bab833860dc5bf86a
alexm <alex-m@outlook.com>
2023-08-14 17:35:14 -0700

    Rewrite App component with functions and comment out class version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
 import JokeList from "./JokeList";
 
 
-/** App component. Renders list of jokes. */
+/** App component (class version). Renders list of jokes. */
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <JokeList />
-            </div>
-        );
-    }
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="App">
+//                 <JokeList />
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
+
+
+/**
+ * App component (functional version). Renders list of jokes.
+ */
+function App () {
+    return (
+        <div className="App">
+            <JokeList />
+        </div>
+    );
 }
+
 
 export default App;
