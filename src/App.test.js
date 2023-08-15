b9c4ce5882252ebb69d161e362b440421a91133e
alexm <alex-m@outlook.com>
2023-08-14 17:39:02 -0700

    Add smoke test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+
+
+test("Renders without crashing", () => {
+    render(<App />);
+});
